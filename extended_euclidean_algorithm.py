4a19edc50b63b4b5073238efb3bc04a16d165e7c
NataliaGuruleva <71883294+NataliaGuruleva@users.noreply.github.com>
2020-11-22 23:27:24 +0300

    Update extended_euclidean_algorithm.py

--- extended_euclidean_algorithm.py
+++ extended_euclidean_algorithm.py
@@ -1,22 +1,23 @@
-a = int(input('Первое число'))
-b = int(input('Второе число'))
-x = 1
-y = 0
-x1 = 0
-y1 = 1
-while a != 0 and b != 0:
-    q = a // b
-    r = a - (b * q)
-    c = x - (x1 * q)
-    d = y - (y1 * q)
-    a = b
-    b = r
-    x = x1
-    y = y1
-    x1 = c
-    y1 = d
-gcd = b
-print(gcd, x, y)
+def egcd(a, b):
+    x = 1
+    y = 0
+    x1 = 0
+    y1 = 1
+    while a != 0 and b != 0:
+        q = a // b
+        r = a - (b * q)
+        c = x - (x1 * q)
+        d = y - (y1 * q)
+        a = b
+        b = r
+        x = x1
+        y = y1
+        x1 = c
+        y1 = d
+    gcd = b
+    return(gcd, x, y)
+# Пример    
+print( egcd(98, 5) )
     
 
 
