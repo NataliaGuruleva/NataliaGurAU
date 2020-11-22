3bc707839ded8580b952dbc75cb81a5ddb5d7a62
NataliaGuruleva <71883294+NataliaGuruleva@users.noreply.github.com>
2020-11-22 23:28:22 +0300

    Update euclidean_algorithm.py

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,14 +1,15 @@
-a = int(input('Первое число'))
-b = int(input('Второе число'))
-if b > a:
-    m = a
-    a = b
-    b = m
-r = a % b
-while(r != 0):
-    a = b
-    b = r
-    q = a / b
-    r = a - (q * b)
-gcd = b
-print(gcd)
+def gcd(a, b):
+    if b > a:
+        m = a
+        a = b
+        b = m
+    r = a % b
+    while(r != 0):
+        a = b
+        b = r
+        q = a // b
+        r = a - (q * b)
+    return b
+# Примеры    
+print( gcd(180, 150) )
+print( gcd(98, 5) )
